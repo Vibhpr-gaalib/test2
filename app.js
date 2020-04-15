23891c62f5cae5bdf93270834f1e8e832bac5b7b
Gaalib <krityamkarma858041@gmail.com>
2020-04-15 05:56:17 -0700

    sorry

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const parser = require('body-parser');
 app.use(parser.urlencoded({extended:false}));
 const port = process.env.PORT || 3000
 app.set('view engine',"ejs");
-const http = require('http').createServer(app);
-var da  = require('./new');
-var address = "Ghumarwin";
 
 
-// da.apirequest(address) 
+
+
+
+
 
 app.get("/",(req,res)=>{
 res.render("home");
