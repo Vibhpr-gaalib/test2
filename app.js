b1287f1f7f12d742aa9f95a11da780aff252f237
Gaalib <krityamkarma858041@gmail.com>
2020-04-15 05:46:05 -0700

    heroku test

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ const app = express();
 const parser = require('body-parser');
 
 app.use(parser.urlencoded({extended:false}));
-
+const port = process.env.PORT || 3000
 app.set('view engine',"ejs");
 const http = require('http').createServer(app);
 var da  = require('./new');
 var address = "Ghumarwin";
 
 
-da.apirequest(address) 
+// da.apirequest(address) 
 
+app.get("/",(req,res)=>{
+res.render("home");
+})
 
 //converting data into to the object
 const data = function(value ){
  const d = JSON.parse(value);
   return d;
 }
+
+app.listen(port,()=>{
+  console.log("started");
+})
